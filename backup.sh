bbfdef3e885fd5dd3fd9edf84f12ddb4a2253a27
Sreenivasarao Gopisetti <Sreenivasa.Raogopisetti@SGS-DEV-LTA-SGOPISETTI.local>
2017-10-11 15:46:10 -0400

    Initial commit

---
+++ backup.sh
@@ -0,0 +1,217 @@
+#!/bin/bash
+# Backup vision resoruces
+# 1. Mongodb - /opt/backups/mongodb
+# 2. Images  - /opt/backups/data
+
+# Default backups are disabled - enabled in props file
+# Default BACKUP_MODE LOCAL - backups stored in a local file system
+# Location can be configure in a props file
+
+# Global variables
+
+BACKUP_MODE=LOCAL
+BACKUP_DB_ENABLED=FALSE
+BACKUP_IMAGES_ENABLED=FALSE
+BACKUP_ROOT="/opt/backups"
+BACKUP_HOME=$BACKUP_ROOT/bin
+BACKUP_CONFIG_FILE=$BACKUP_HOME/props
+BACKUP_LOCATION="/opt/backups"
+KEEP_LOCAL_COPY="TRUE"
+
+DATA_SOURCE_FOLDER="/opt/tomcat/webapps/data/"
+
+
+MONGODB_DATABASE="vision_local"
+MONGODB_USER="sgs"
+MONGODB_PASSWORD="RF1Dkings"
+MONGODB_HOST="localhost"
+MONGODB_PORT="27017"
+
+SFTP_USER=""
+SFTP_HOST=""
+SFTP_TARGET=""
+
+cleanup_remote() {
+echo "Cleanup remote file->"
+sftp $1@$2 >>$BACKUP_ROOT_LOGS/remote.logs <<EOF
+cd $3
+rm $4 
+bye
+EOF
+echo "<-Cleanup remote file"
+}
+
+#####################################
+# File transfer - 
+# Requires pre authentication setup
+# Ref : key setup doc
+#####################################
+file_transfer(){
+	echo "SFTP file->"
+	USER=$SFTP_USER   #admin
+	SFTP_SERVER=$SFTP_HOST   #192.168.4.19
+	TARGET_BACKUP_LOCATION=$SFTP_TARGET  #"/opt/backups/backup_192_168_5_10/"
+	SOURCE_FILE=$1
+	
+sftp $USER@$SFTP_SERVER >>$BACKUP_ROOT_LOGS/remote.logs <<EOF
+cd $TARGET_BACKUP_LOCATION
+put $SOURCE_FILE
+bye
+EOF
+rc=$?
+	if [ $rc -ne 0 ]; then
+	 cleanup_remote $USER $SFTP_SERVER $TARGET_BACKUP_LOCATION $SOURCE_FILE
+	 return 1
+	else
+	 return 0
+	fi
+	echo "<-SFTP file"
+}
+#####################################
+#  Mongodb backup
+#####################################
+mongodb_backup(){
+	### Set server settings
+	HOST=$MONGODB_HOST
+	PORT=$MONGODB_PORT # default mongoDb port is 27017
+	USERNAME=$MONGODB_USER
+	PASSWORD=$MONGODB_PASSWORD
+	DATABASE=$MONGODB_DATABASE
+	
+	CURRENT_DAY=`date +%y%m%d%H%M%S`
+	BACKUP_MONGODB_PATH_CURRENT_DAY=$BACKUP_MONGODB_PATH/$CURRENT_DAY
+	
+	mkdir -p $BACKUP_MONGODB_PATH_CURRENT_DAY
+	
+	mongodump -h $HOST -d $DATABASE -u $USERNAME -p $PASSWORD -o $BACKUP_MONGODB_PATH_CURRENT_DAY
+	
+	cd $BACKUP_MONGODB_PATH
+	
+	TAR_FILE=$CURRENT_DAY.mongodb.tar.gz
+	
+	tar -zcvf $TAR_FILE $CURRENT_DAY
+	rm -r $CURRENT_DAY
+	
+	if [ "$BACKUP_MODE" = "REMOTE" ]; then
+	   file_transfer $BACKUP_MONGODB_PATH/$TAR_FILE
+	   ret=$?
+	   if [ $ret == 0 ]; then
+	   	  if [ "$KEEP_LOCAL_COPY" != "TRUE" ]; then
+	   	  		rm $BACKUP_MONGODB_PATH/$TAR_FILE
+	   	  fi
+	   fi
+	fi
+
+}
+
+data_backup() {
+
+
+	CURRENT_DAY=`date +%y%m%d%H%M%S`
+	mkdir $BACKUP_DATA_PATH/$CURRENT_DAY
+
+	BACKUP_DATA_PATH_CURRENT_DAY=$BACKUP_DATA_PATH/$CURRENT_DAY
+	
+	echo "$DATA_SOURCE_FOLDER"
+	echo "$BACKUP_DATA_PATH_CURRENT_DAY"
+	
+	cp -R $DATA_SOURCE_FOLDER $BACKUP_DATA_PATH_CURRENT_DAY
+	
+	cd $BACKUP_DATA_PATH
+	TAR_FILE=$CURRENT_DAY.data.tar.gz
+	tar -zcvf $TAR_FILE $CURRENT_DAY
+	rm -r $CURRENT_DAY
+	if [ "$BACKUP_MODE" = "REMOTE" ]; then
+	file_transfer $BACKUP_DATA_PATH/$TAR_FILE
+	   ret=$?
+		if [ "$KEEP_LOCAL_COPY" != "TRUE" ]; then
+			rm $BACKUP_MONGODB_PATH/$TAR_FILE
+		fi
+	fi
+}
+
+# check configuration props exits under the run directory /opt/backups/bin/
+if [ ! -f "$BACKUP_CONFIG_FILE" ]; then
+	echo "Configuration file missing.."
+	exit 0
+fi
+
+while IFS=" " read -r key value
+do
+	if [ "$key" = "BACKUP_IMAGES_ENABLED" ]; then
+		BACKUP_IMAGES_ENABLED="$value"
+	fi
+	if [ "$key" = "BACKUP_DB_ENABLED" ]; then
+		BACKUP_DB_ENABLED="$value"
+	fi
+	if [ "$key" = "BACKUP_LOCATION" ]; then
+		BACKUP_LOCATION="$value"
+	fi
+	if [ "$key" = "BACKUP_MODE" ]; then
+		BACKUP_MODE="$value"
+	fi
+	if [ "$key" = "MONGODB_USER" ]; then
+		MONGODB_USER="$value"
+	fi
+	if [ "$key" = "MONGODB_PASSWORD" ]; then
+	  MONGODB_PASSWORD="$value"
+	fi
+	if [ "$key" = "MONGODB_HOST" ]; then
+	  MONGODB_HOST="$value"
+	fi	
+	if [ "$key" = "MONGODB_PORT" ]; then
+	  MONGODB_PORT="$value"
+	fi
+	if [ "$key" = "MONGODB_DATABASE" ]; then
+	  MONGODB_DATABASE="$value"
+	fi
+	if [ "$key" = "DATA_SOURCE_FOLDER" ]; then
+	  DATA_SOURCE_FOLDER="$value"
+	fi
+	if [ "$key" = "KEEP_LOCAL_COPY" ]; then
+	  KEEP_LOCAL_COPY="$value"
+	fi	
+	if [ "$key" = "KEEP_LOCAL_COPY" ]; then
+	  KEEP_LOCAL_COPY="$value"
+	fi
+	if [ "$key" = "SFTP_USER" ]; then
+	  SFTP_USER="$value"
+	fi
+	if [ "$key" = "SFTP_HOST" ]; then
+	  SFTP_HOST="$value"
+	fi
+	if [ "$key" = "SFTP_TARGET" ]; then
+	  SFTP_TARGET="$value"
+	fi
+	echo "$key $value" 
+done < "$BACKUP_CONFIG_FILE"
+if [ "$BACKUP_DB_ENABLED" = "FALSE" ] && [ "$BACKUP_IMAGES_ENABLED" = "FALSE" ]
+then
+    echo "Backup not enabled"
+    exit 0  
+fi
+
+if [ -z "$BACKUP_LOCATION" ];then
+	BACKUP_LOCATION=BACKUP_ROOT
+fi
+
+BACKUP_MONGODB_PATH=$BACKUP_LOCATION/mongodb
+if [ ! -d $BACKUP_MONGODB_PATH ]; then
+ 	mkdir -p $BACKUP_MONGODB_PATH
+fi
+
+BACKUP_DATA_PATH=$BACKUP_LOCATION/data
+if [ ! -d $BACKUP_DATA_PATH ]; then
+	mkdir -p $BACKUP_DATA_PATH
+fi
+
+BACKUP_DATE_LOGS=$BACKUP_LOCATION/logs
+if [ ! -d $BACKUP_DATE_LOGS ]; then
+	mkdir $BACKUP_DATE_LOGS
+fi
+if [ "$BACKUP_DB_ENABLED" == "TRUE" ]; then
+	mongodb_backup
+fi
+if [ "$BACKUP_IMAGES_ENABLED" == "TRUE" ]; then
+	data_backup
+fi
---
+++ restore.sh
@@ -0,0 +1,372 @@
+#!/bin/bash
+# Restore vision resoruces
+
+# Global Variables
+ARGS=""
+VALUE=""
+
+#####################################
+# Read required arguments
+#####################################
+readvalue(){
+
+	flag=1
+	VALUE=""
+	while [ $flag != 0 ]
+	do
+		echo "Enter $ARGS !"
+		read param
+		if [ -z "$param" ]; then
+			flag=1
+		else
+			VALUE=$param
+			flag=0
+		fi
+	done
+
+}
+#####################################
+#  Mongodb backup
+#####################################
+mongodb_backup() {
+
+	echo "mongodb_backup->"
+	CURRENT_DAY=`date +%y%m%d%H%M%S`
+	BACKUP_MONGODB_PATH_CURRENT_DAY=$6/$CURRENT_DAY
+	mkdir -p $BACKUP_MONGODB_PATH_CURRENT_DAY
+	
+	#mongodump -h $HOST -d $DATABASE -u $USERNAME -p $PASSWORD -o $BACKUP_MONGODB_PATH_CURRENT_DAY
+	mongodump --host $1 --port $2 --db $3 --username $4 --password $5 --out $BACKUP_MONGODB_PATH_CURRENT_DAY
+	
+	echo "Mongodb backup location $BACKUP_MONGODB_PATH_CURRENT_DAY."
+	echo "<-mongodb_backup"
+	
+}
+#####################################
+# Restoring Mongodb 
+#####################################
+mongodb_restore_backup() {
+	echo "mongodb_restore_backup->"
+	#mongorestore --host $HOST --port $PORT --username $USERNAME --db vision_local --password $PASSWORD $RESTORE_PATH
+	mongorestore --drop --host $1 --port $2 --db $3 --username $4 --password $5 $6
+	if [ $? -ne 0 ]; then
+	   echo "Mongodb restore failed... "
+	fi
+	echo "<-mongodb_restore_backup"
+}
+
+#####################################
+# Stop Tomcat
+#####################################
+stop_tomcat() {
+	echo "stop_tomcat->"
+	service tomcat stop
+	echo "stop tomcat process..."
+	stopped=1
+	running="running"
+	count=0
+	while [ $stopped != 0 ]
+	do
+	    ret=$(service tomcat status | grep running)
+	    if [[ "$ret" =~ "$running" ]]; then
+	        echo "waiting to stop tomcat..."
+	        sleep 5
+	        ((count++))
+	        echo "$count"
+	        stopped=1
+	    else
+	        echo "Tomcat stopped. Continue restore."
+	        stopped=0
+	    fi
+	    if [ $count -gt 5 ]; then
+	        echo "Timeout unable to stop in 25 seconds."
+	    fi
+	done
+echo "<-stop_tomcat"
+}
+
+#####################################
+# Stop Replenishment
+#####################################
+stop_replenishment() {
+	echo "stop_replenishment->"
+	sudo service replenishment stop
+	echo "stop replenishment process..."
+	stopped=1
+	running="running"
+	count=0
+	while [ $stopped != 0 ]
+	do
+	    ret=$(sudo service replenishment status | grep running)
+	    if [[ "$ret" =~ "$running" ]]; then
+	        echo "waiting to stop replenishment..."
+	        sleep 5
+	        ((count++))
+	        stopped=1
+	    else
+	        echo "Replenishment stopped. Continue restore."
+	        stopped=0
+	    fi
+	    if [ $count -gt 5 ]; then
+	        echo "Timeout unable to stop in 25 seconds."
+	    fi
+	done
+	echo "<-stop_replenishment"
+}
+
+#####################################
+# Start Tomcat
+#####################################
+start_tomcat() {
+	echo "start_tomcat->"
+	service tomcat start
+	echo "start tomcat process..."
+	started=1
+	running="running"
+	count=0
+	while [ $started != 0 ]
+	do
+	    ret=$(service tomcat status | grep running)
+	    if [[ "$ret" =~ "$running" ]]; then
+	        echo "Tomcat started. Continue restore."
+	        started=0
+	    else
+	        echo "waiting to start tomcat..."
+	        sleep 5
+	        ((count++))
+	        echo "$count"
+	        started=1
+	    fi
+	    if [ $count -gt 12 ]; then
+	        echo "Timeout unable to start in 60 seconds..."
+	    fi
+	done
+	echo "<-start_tomcat"
+}
+#####################################
+# Start Replenishment
+#####################################
+start_replenishment() {
+	echo "start_replenishment->"
+	service replenishment start
+	echo "start replenishment process..."
+	started=1
+	running="running"
+	count=0
+	while [ $started != 0 ]
+	do
+	    ret=$(service replenishment status | grep running)
+	    if [[ "$ret" =~ "$running" ]]; then
+	        echo "Replenishment started. Continue restore."
+	        started=0
+	    else
+	        echo "waiting to start replenishment..."
+	        sleep 5
+	        ((count++))
+	        echo "$count"
+	        started=1
+	    fi
+	    if [ $count -gt 12 ]; then
+	        echo "Timeout unable to start in 60 seconds..."
+	    fi
+	done
+	echo "<-start_replenishment"
+}
+#####################################
+# Restore Mongodb
+#####################################
+mongodb_restore(){
+
+ARGS="mongodb username"
+readvalue
+if [ -z "$VALUE" ]; then
+    echo "Invalid $ARGS $VALUE. "
+    exit 0
+fi
+username=$VALUE
+
+ARGS="mongodb user $username password"
+readvalue
+if [ -z "$VALUE" ]; then
+    echo "Invalid $ARGS $VALUE. "
+    exit 0
+fi
+password=$VALUE
+
+ARGS="mongodb host"
+readvalue
+if [ -z "$VALUE" ]; then
+    echo "Invalid $ARGS $VALUE. "
+    exit 0
+fi
+host=$VALUE
+
+
+ARGS="mongodb port"
+readvalue
+if [ -z "$VALUE" ]; then
+    echo "Invalid $ARGS $VALUE. "
+exit 0
+fi
+port=$VALUE
+
+ARGS="mongodb database"
+readvalue
+if [ -z "$VALUE" ]; then
+    echo "Invalid $ARGS $VALUE. "
+    exit 0
+fi
+database=$VALUE
+backup_path="/tmp"
+echo "---------------------------------------------"
+echo "Verify parameters and confirm y to continue !"
+echo "mongodb username = $username"
+echo "password = $password"
+echo "host = $host"
+echo "database = $database"
+echo "---------------------------------------------"
+ARGS=" y to continue n to exit "
+readvalue
+if [ "$VALUE" = "n" ]; then
+   echo "Mongodb restore process stopped by the user. "
+   exit 0
+fi
+ARGS="Re-confirm y to continue n to exit "
+readvalue
+if [ "$VALUE" = "n" ]; then
+   echo "Mongodb restore process stopped by the user. "
+   exit 0
+fi
+
+#Verify backupd directory exits with database name
+RESTORE_DB_DIR=$RESTORE_FILE/$database
+echo "$RESTORE_DB_DIR"
+if [ ! -d "$RESTORE_DB_DIR" ]; then
+    echo "Restore directory is not matching with the database name"
+    echo "Exit restore."
+    exit 0
+fi
+
+echo "Backup database before restore: starts "
+mongodb_backup $host $port $database $username $password $backup_path
+echo "Backup database before restore: ends "
+
+echo "Shutdown services: starts"
+stop_tomcat
+stop_replenishment
+echo "Shutdown services: ends"
+
+mongodb_restore_backup $host $port $database $username $password $RESTORE_DB_DIR
+
+start_tomcat
+start_replenishment
+}
+#####################################
+# Restore resources 
+#####################################
+backup_images() {
+	cp -R $1 /tmp
+	echo "Backed up images before restore to /tmp"
+}
+
+#####################################
+# Restore resources 
+#####################################
+restore_images() {
+	ARGS="Image target folder (enter y to use default /opt/tomcat/webapps/data )"
+	readvalue
+	if [ -z "$VALUE" ]; then
+		echo "Invalid $ARGS $VALUE. "
+		exit 0;
+	fi
+	if [ "$VALUE" = "y" ]; then
+		VALUE="/opt/tomcat/webapps/data"
+    fi
+	target=$VALUE
+	backup_images $target
+	rc=$?
+	if [ $rc -ne 0 ]; then
+		echo "backup images failed, exit"
+		exit 0;
+	fi
+	rm -R /opt/tomcat/webapps/data
+	mv $RESTORE_FILE/data $target
+	echo "Images restored"
+}
+
+
+#####################################
+# Main 
+#####################################
+RESTORE_OPTION=$1
+RESTORE_FILE=""
+
+RESTORE_PATH=$2
+
+if [[ $(id -u) -ne 0 ]] ; then 
+	echo "Please run as root" ; 
+	exit 1 ; 
+fi
+
+if [ "$RESTORE_OPTION" != "database" ] && [ "$RESTORE_OPTION" != "images" ]
+then
+    echo "Run from backup file directory [backup file path relative]." 
+    echo "Usage /opt/backups/bin/restore <database or images> <171010141414.mongodb.tar.gz or 171010141414.data.tar.gz>"
+    exit 0
+fi
+if [ "$RESTORE_OPTION" = "database" ]; then
+    if [ ! -f $RESTORE_PATH ] && [ ! -d $RESTORE_PATH ]
+    then
+        echo "Restore resource not found $RESTORE_PATH"
+        exit 0
+    else
+
+        if [ -f $RESTORE_PATH ]; then
+            if [[ "$RESTORE_PATH" =~ ".mongodb.tar.gz" ]]; then
+                RSTR=""
+                RESTORE_FILE=${RESTORE_PATH/.mongodb.tar.gz/$RSTR}
+                echo "Untar backup ...$RESTORE_FILE"
+                tar xzf $RESTORE_PATH -C ./
+                if [ $? -ne 0 ];then
+                   echo "Untar file failed !"
+                   exit 0;
+                fi
+            else
+                echo "Corrupted or Invalid backup !"
+                exit 0
+            fi
+        fi
+        if [ -d "$RESTORE_PATH" ]; then
+           RESTORE_FILE=$RESTORE_PATH
+        fi
+        mongodb_restore
+    fi
+fi
+
+if [ "$RESTORE_OPTION" = "images" ]; then
+    if [ ! -f $RESTORE_PATH ] && [ ! -d $RESTORE_PATH ]
+    then
+        echo "Restore resource not found $RESTORE_PATH"
+        exit 0
+    else
+ 		if [ -f $RESTORE_PATH ]; then
+            if [[ "$RESTORE_PATH" =~ ".data.tar.gz" ]]; then
+                RSTR=""
+                RESTORE_FILE=${RESTORE_PATH/.data.tar.gz/$RSTR}
+                echo "Untar backup ...$RESTORE_FILE"
+                tar xzf $RESTORE_PATH -C ./
+                if [ $? -ne 0 ];then
+                   echo "Untar file failed !"
+                   exit 0;
+                fi
+            else
+                echo "Corrupted or Invalid backup !"
+                exit 0
+            fi
+        fi
+        if [ -d "$RESTORE_PATH" ]; then
+           RESTORE_FILE=$RESTORE_PATH
+        fi
+        restore_images
+    fi
+fi
